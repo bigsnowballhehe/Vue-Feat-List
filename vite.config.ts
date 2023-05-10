29141c70e1736137b9960b11b253943291e8ec8d
Snlan <pridewui@foxmail.com>
2023-05-10 15:46:16 +0800

    fix:try config

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ import Pages from 'vite-plugin-pages'
 import Layouts from 'vite-plugin-vue-layouts'
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/VueAPI/',
+  base: './',
   plugins: [
     vue(),
     Pages(),
