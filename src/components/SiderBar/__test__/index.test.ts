8f4255f8d8df41f307b91b3596e11ff7e63138a2
Snlan <pridewui@foxmail.com>
2023-05-29 15:17:52 +0800

    test: add test

--- src/components/SiderBar/__test__/index.test.ts
+++ src/components/SiderBar/__test__/index.test.ts
@@ -3,21 +3,22 @@ import { mount } from '@vue/test-utils'
 import SiderBar from '../index.vue'
 
 describe('test SiderBarvue', () => {
-  test('should mount success ', () => {
-    const pathList = new Map()
-    pathList.set('macros', new Set().add({
-      name: 'macros-props',
-      path: '/macros/props',
-      props: true,
-      meta: {
-        title: 'V-model in components',
-      },
-    }))
-    const wrapper = mount(SiderBar, {
-      props: {
-        routerItems: pathList,
-      },
-    })
+  const pathList = new Map()
+  pathList.set('macros', new Set().add({
+    name: 'macros-props',
+    path: '/macros/props',
+    props: true,
+    meta: {
+      title: 'V-model in components',
+    },
+  }))
+  const wrapper = mount(SiderBar, {
+    props: {
+      routerItems: pathList,
+    },
+  })
+  test('test routerItems', () => {
     expect(wrapper.find('.p-2').text()).toEqual('V-model in components')
+    expect(wrapper.find('li span').text()).toEqual('macros:')
   })
 })
